4eec0998f4659a06f72a54cac0fe81482f108e7d
Christopher Pina <49688431+Christopherpina2022@users.noreply.github.com>
2026-01-30 16:42:50 -0600

    Added Gamestate Global group + basic save functionality

--- Scripts/json_parser.gd
+++ Scripts/json_parser.gd
@@ -1,9 +1,24 @@
 extends Node
 
-func getData(path) -> void:
-	var json = JSON.new()
-	if json.data == null:
-		print("Error: Could not read JSON file")
+func saveGame(path: String) -> void:
+	var saveFile = FileAccess.open(path, FileAccess.WRITE)
+	var saveNodes = get_tree().get_nodes_in_group("gameState")
+	for node in saveNodes:
+		if node.scene_file_path.is_empty():
+			continue
+		
+		if !node.has_method("save"):
+			continue
+		
+		var nodeData = node.call("save")
+		var jsonString = JSON.stringify(nodeData)
+		
+		saveFile.store_line(jsonString)
+
+func getSave(path: String) -> void:
+	if  not FileAccess.file_exists(path):
+		print("Error: JSON path " + path + " is not in a valid path")
 		pass
 	
+	# set game state to what we saved it to
 	pass
