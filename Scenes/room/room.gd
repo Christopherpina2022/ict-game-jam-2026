2915ab29a0c41c2646aba6bd8a5fc1c14702c2c6
samuscide <gwilkinson0@gmail.com>
2026-01-31 15:33:15 -0600

    added core game state dicts to room manager
    added skeleton for room and npc data storage and use

--- Scenes/room/room.gd
+++ Scenes/room/room.gd
@@ -9,6 +9,10 @@ extends Node2D
 var _using_a := true
 var _is_fading := false
 
+signal movementButtonSignal
+
+var activeJunction = RoomManager.Juction
+
 func _ready() -> void:
 	crossFade(RoomManager.roomLibrary["reception"])
 	# start the options menu in UI
@@ -16,6 +20,7 @@ func _ready() -> void:
 	if $Parallax2D.characterCount == 0:
 		UI.disableNode($GameUi/options/optionList/communicate)
 	UI.revealNode(UI.options)
+<<<<<<< Updated upstream
 
 func crossFade(texture: Texture2D):
 	if _is_fading:
@@ -37,3 +42,7 @@ func crossFade(texture: Texture2D):
 	from.visible = false
 	_using_a = !_using_a
 	_is_fading = false
+=======
+	
+	
+>>>>>>> Stashed changes
